d968e38ae361193a5a36d4e3d5cffb52ed9e2c16
davidemarcoli <davide@marcoli.ch>
2023-12-30 22:20:49 +0100

    Initial Commit

---
+++ streaming/src/fetch-movies.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOWIwMjdmNDU3NDY0NDcwMzg4MmQyODc4ZjllYWM4NSIsInN1YiI6IjY1OTA3ZTE0MDI4ZjE0MjRjZGM1NGJjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PhVtCDsxtVhvVnkuyiLxaJd3izjA8OcE_b0E0fwhx1E'
+    }
+};
+
+fetch('https://api.themoviedb.org/3/discover/movie?language=en-US&page=1&sort_by=popularity.desc', options)
+    .then(response => response.json())
+    .then(response => {
+        if (!response) {
+            console.error("No results found")
+            return
+        }
+        fs.writeFileSync('movies.json', JSON.stringify(response.results, null, 2));
+        console.log(response)
+    })
+    .catch(err => console.error(err));
